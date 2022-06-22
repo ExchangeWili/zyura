218ec48533190fc1a1bdcf9336fc77702f26bc6c
ExhangeHanteng <107592734+ExhangeHanteng@users.noreply.github.com>
2022-06-22 11:24:17 +0700

    Add files via upload

--- plugins/ainetoxic.js
+++ plugins/ainetoxic.js
@@ -1,19 +1,34 @@
 let fs = require('fs')
-const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn }) => {
-let gaboleh = fs.readFileSync('./mp3/PTT-20220220-WA0414.opus')
-let ihhomkasar = fs.readFileSync('./mp3/WhatsApp-Audio-2021-06-16-at-14.35.33.opus')
-conn.sendFile(m.chat, ihhomkasar, '', '', m, true)
-setTimeout(() => {
-conn.sendFile(m.chat, gaboleh, '', '', m, true)
-}, 1000)
-//conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
-// await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = m => m
+
+handler.all = async function (m, { isBlocked }) {
+    if (isBlocked) return
+
+   let regs = /(memek)/i
+   let isVertexThai = regs.exec(m.text)
+   if (isVertexThai && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
+   let regk = /(kontol)/i
+   let isVertexSymbol = regk.exec(m.text)
+   if (isVertexSymbol && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
+   let regp = /(bangsat)/i
+   let isVertexsThai= regp.exec(m.text)
+   if (isVertexsThai && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }   
+   // tambahin sendiri code virus WhatsApp..
 }
-
-handler.customPrefix = /^(kontol|memek|jembut|jancok|ngentod)$/i
+handler.customPrefix = /^(kontol|memek|jembut|jancok|ngentod|anjing|bangsat|bngst|anjg)$/i
 handler.command = new RegExp
-
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
 handler.limit = true
 handler.mods = false 
 handler.premium = false
