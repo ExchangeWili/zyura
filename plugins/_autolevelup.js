f38a8503dd72b7520292ba687c4952c155916e30
ExhangeHanteng <107592734+ExhangeHanteng@users.noreply.github.com>
2022-06-29 08:51:50 +0700

    Add files via upload

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -24,8 +24,8 @@ let levelling = require('../lib/levelling')
 module.exports = {
 	before(m) {
 		let user = global.db.data.users[m.sender]
-		if (!user.autolevelup) return !900000
-		let before = user.level * 900000
+		if (!user.autolevelup) return !0
+		let before = user.level * 1
 		while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 
 		if (before !== user.level) {
